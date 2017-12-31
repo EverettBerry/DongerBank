262a04beddc34c48d3426de5a873875d4e065c73
Everett Berry <epberry@purdue.edu>
2017-12-31 18:50:03 -0500

    setup for DAO

---
+++ contracts/DongerBank.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.16;
+
+contract owned {
+    address public owner;
+
+    function owned()  public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner  public {
+        owner = newOwner;
+    }
+}
+
+contract tokenRecipient {
+    event receivedEther(address sender, uint amount);
+    event receivedTokens(address _from, uint256 _value, address _token, bytes _extraData);
+
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public {
+        Token t = Token(_token);
+        require(t.transferFrom(_from, this, _value));
+        receivedTokens(_from, _value, _token, _extraData);
+    }
+
+    function () payable  public {
+        receivedEther(msg.sender, msg.value);
+    }
+}
+
+interface Token {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+}
+
+contract DongerBank is owned, tokenRecepient {
+}
+
